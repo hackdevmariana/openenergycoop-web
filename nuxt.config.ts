2e92ac588b47b47fdf2d41e244848dc3af4023b6
Fernando <fernando@juandemariana.org>
2025-08-29 11:51:20 +0200

    Add nuxtjs/pwa to project.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/pwa'],
+  pwa: {
+    manifest: {
+      name: 'Electricity Price Web',
+      short_name: 'PriceWeb',
+      lang: 'es',
+      theme_color: '#ffffff'
+    }
+  }
 })
