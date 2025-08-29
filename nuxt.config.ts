fcea28ad4d14713e7feff1d364b3dfc88e8d0a7f
Fernando <fernando@juandemariana.org>
2025-08-29 11:56:25 +0200

    Add Tailwindcss to project.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,8 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/eslint',
     '@pinia/nuxt',
-    '@sidebase/nuxt-auth'
+    '@sidebase/nuxt-auth',
+    '@nuxtjs/tailwindcss',
   ],
   pwa: {
     manifest: {
