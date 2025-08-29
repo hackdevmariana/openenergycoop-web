5de4a74da89cb8694684264147e267339215014b
Fernando <fernando@juandemariana.org>
2025-08-29 11:55:12 +0200

    Add vueuse/head to project.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,5 +15,11 @@ export default defineNuxtConfig({
       lang: 'es',
       theme_color: '#ffffff'
     }
-  }
+  },
+  app: {
+    head: {
+      title: 'Open Energy Coop Web',
+      meta: [{ name: 'description', content: 'Open Energy Coop Web' }],
+    },
+  },
 })
